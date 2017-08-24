77a6e624992039e83e65cc17a90f0f84be1d4f26
Chelovek760 <Chelovek760@gmail.com>
2017-08-24 16:33:35 +0300

    web_blog_v3

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-sudo pip install --upgrade django
-sudo pip install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf hello:app
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
