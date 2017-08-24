fe8bd7c2ae8af7bda6ff92836081419bf891c529
Chelovek760 <Chelovek760@gmail.com>
2017-08-24 14:21:09 +0300

    web_blog

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:app
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
