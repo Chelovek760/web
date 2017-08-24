12058b9b41a791f91392fcfe94ff2d34e290c1c3
Chelovek760 <Chelovek760@gmail.com>
2017-08-24 16:27:01 +0300

    web_blog_v3

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo pip install --upgrade django
+sudo pip install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
