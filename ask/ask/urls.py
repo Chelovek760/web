a1ed49778a6bf618cd611fc6ed829c58ab879ef1
chelovek760 <chelovek760@gmail.com>
2018-02-17 20:14:39 +0300

    v.5

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^question/<\d+>/', test),
+    url(r'^question/(?P<quest_id>[0-9]+)/$', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
     url(r'^new/', test),
--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi.application
+sudo ln -sf gunicorn /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf gunicorn /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
