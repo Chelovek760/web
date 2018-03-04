aabc37e0473511e7dfb93c45cdecfbe926481c57
Chelovek760 <Chelovek760@gmail.com>
2018-03-04 22:28:54 +0300

    web_blog_v6.5

--- init.sh
+++ init.sh
@@ -1,7 +1,18 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#!/bin/bash
+
+# Install
+#sudo pip install django-autofixture pytz
+#sudo apt-get install -y w3m
+
+# Nginx
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+touch /home/box/nginx.log
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf gunicorn /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf gunicorn /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+
+# Gunicorn (ver. 17.5)
+touch /home/box/gunicorn.log
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-sudo pip install pymysql
