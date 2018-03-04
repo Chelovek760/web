b31f33b9feecbba19e7c0449971193e00bbf720c
Chelovek760 <Chelovek760@gmail.com>
2018-03-04 15:25:23 +0300

    web_blog_v6.1

--- sql.sh
+++ sql.sh
@@ -3,6 +3,5 @@ mysql -uroot -e "CREATE USER 'admin'@'localhost'"
 mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('pass111')"
 mysql -uroot -e "CREATE DATABASE mybase"
 mysql -uroot -e "GRANT ALL ON mybase.* TO 'admin'@'localhost'"
-cd ~ /home/box/web/aks/
-sudo python manage.py makemigrations qa
-sudo python manage.py migrate
+sudo python ~/home/box/web/aks/manage.py makemigrations qa
+sudo python ~/home/box/web/aks/manage.py migrate
