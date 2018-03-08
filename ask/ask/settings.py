17aa140a9ae7352d037eb416c606afb997d10e91
Chelovek760 <Chelovek760@gmail.com>
2018-03-08 13:08:31 +0300

    oh

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -53,21 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ask.urls'
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+
 
 WSGI_APPLICATION = 'ask.wsgi.application'
 
@@ -118,3 +104,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'ask/../templates'),)
