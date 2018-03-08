247a58300b9019d46f10175b162ffa7cb977b407
Chelovek760 <Chelovek760@gmail.com>
2018-03-08 13:28:56 +0300

    oh

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import url
-from qa.views import question_answer, new
-from qa.views import test, question, question_detail, popular
-from qa.views import user_signup, user_login, user_logout
+from qa.views import new
+from qa.views import question, popular
+
 
 urlpatterns = [
     url(r'^$', new, name='new'),
-    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
+    # url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
     url(r'^popular/', popular, name='popular'),
     url(r'^ask/', question, name='question'),
-    url(r'^answer/', question_answer, name='question_answer'),
-    url(r'^signup/', user_signup, name='signup'),
-    url(r'^login/', user_login, name='login'),
-    url(r'^logout/', user_logout, name='logout'),
-    url(r'^new/', test, name='new'),
+    # url(r'^answer/', question_answer, name='question_answer'),
+    # url(r'^signup/', user_signup, name='signup'),
+    # url(r'^login/', user_login, name='login'),
+    # rl(r'^logout/', user_logout, name='logout'),
+    # url(r'^new/', test, name='new'),
 ]
