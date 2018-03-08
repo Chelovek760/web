3b902f81dea279526ee8be36480768193e7527fa
Chelovek760 <Chelovek760@gmail.com>
2018-03-08 12:09:50 +0300

    web_blog_v6(re)

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,7 @@
 """ask URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,26 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from qa.views import new,popular,question
 
-from ask.views import found, not_found, init25
-from qa.views import index, popular, ask, login_view, signup
 
 urlpatterns = [
 
-    url(r'^$', index),
+    url(r'^$', new),
 
-    url(r'^init25/', init25),
-    url(r'^login/', login_view),
-    url(r'^signup/', signup),
-    url(r'^ask/', ask),
+   # url(r'^init25/', init25),
+   # url(r'^login/', login_view),
+   # url(r'^signup/', signup),
+    #url(r'^ask/', ask),
     # url(r'^answer/', answer),
     url(r'^popular/', popular),
-    url(r'^new/', found),
+   # url(r'^new/', found),
 
-    url(r'^admin/', admin.site.urls),
-    url(r'^question/', include('qa.urls')),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^question/^(?P<num>\d+)/$', question),
 
-    url(r'^', not_found),
+    #url(r'^', not_found),
 ]
--- init.sh
+++ init.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-
-# Install
-#sudo pip install django-autofixture pytz
-#sudo apt-get install -y w3m
-
-# Nginx
-if [ -f /etc/nginx/sites-enabled/default ]; then
-  sudo rm /etc/nginx/sites-enabled/default
-fi
-touch /home/box/nginx.log
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-# Gunicorn (ver. 17.5)
-touch /home/box/gunicorn.log
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -sf gunicorn /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf gunicorn /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+sudo pip install pymysql
