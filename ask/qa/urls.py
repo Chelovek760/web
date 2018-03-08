c0a644d5f36cd659bc8f38b9b2d573747171b992
Chelovek760 <Chelovek760@gmail.com>
2018-03-08 10:37:57 +0300

    web_blog_v6.6(views)

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from qa.views import question
+from views import question
 
 urlpatterns = [
     url(r'^(?P<num>\d+)/$', question),
